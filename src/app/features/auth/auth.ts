672a102e55d6880f9a219cfbae128ae02d01a4d2
HBFL3Xx <105460420+HBFLEX@users.noreply.github.com>
2025-11-14 23:30:44 +0200

    update auth ui

--- src/app/features/auth/auth.ts
+++ src/app/features/auth/auth.ts
@@ -1,29 +1,46 @@
 import { Component, inject, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { TuiButton } from '@taiga-ui/core';
-import { TuiInputModule, TuiInputPasswordModule } from '@taiga-ui/legacy';
+import {FormsModule} from '@angular/forms';
+import {
+  TuiLabel,
+  TuiTextfield,
+  TuiTitle,
+  TuiButton,
+  TuiTextfieldComponent, TuiIcon,
+} from '@taiga-ui/core';
 import { AuthService } from './services/auth-service';
+import {TuiForm, TuiHeader} from '@taiga-ui/layout';
+import { NgOptimizedImage } from '@angular/common';
+import {TuiPassword} from '@taiga-ui/kit';
 
 @Component({
   selector: 'app-auth',
-  imports: [FormsModule, TuiButton, TuiInputModule, TuiInputPasswordModule],
+  imports: [
+    NgOptimizedImage,
+    TuiForm,
+    TuiHeader,
+    TuiTitle,
+    TuiLabel,
+    TuiIcon,
+    TuiTextfieldComponent,
+    TuiTextfield,
+    TuiButton,
+    FormsModule,
+    TuiPassword,
+  ],
   templateUrl: './auth.html',
   styleUrl: './auth.less',
 })
 export class Auth {
   private readonly authService = inject(AuthService);
 
-  readonly email = signal('');
-  readonly password = signal('');
-  readonly showPassword = signal(false);
+  email = signal('');
+  password = signal('');
 
   readonly isLoading = this.authService.isLoading;
   readonly error = this.authService.error;
 
   async onSubmit(): Promise<void> {
-    if (!this.email() || !this.password()) {
-      return;
-    }
+    if (!this.email() || !this.password()) return;
 
     try {
       await this.authService.login({
